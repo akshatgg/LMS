5a5a0d6f588177d851de2d1af02f87281b1c0aae
akshatgg <akshatg9636@gmail.com>
2024-04-06 13:25:33 +0545

    adding celeberaties

--- Frontend/src/Components/About/About.jsx
+++ Frontend/src/Components/About/About.jsx
@@ -3,26 +3,44 @@ function About() {
   return (
     <>
       <div className="h-screen bg-[#1D232A]">
-        <div className="flex justify-center items-center max-h-[700px]">
-          <div className="">
-            <p className="text-[#EAB308] text-5xl font-semibold font-serif">
-              Affordable and quality education
-            </p>
-            <p className="text-white font-serif mt-8 text-xl">
-              Our goal is to provide the afoordable and quality education to the
-              world. We are providing the platform
-              <br /> for the aspiring teachers and students to share their
-              skills, creativity and knowledge to each other to
-              <br /> empower and contribute in the growth and wellness of
-              mankind.
-            </p>
+        <div className="flex flex-col">
+          <div className="flex justify-center items-center max-h-[700px]">
+            <div className="">
+              <p className="text-[#EAB308] text-5xl font-semibold font-serif">
+                Affordable and quality education
+              </p>
+              <p className="text-white font-serif mt-8 text-xl">
+                Our goal is to provide the afoordable and quality education to
+                the world. We are providing the platform
+                <br /> for the aspiring teachers and students to share their
+                skills, creativity and knowledge to each other to
+                <br /> empower and contribute in the growth and wellness of+
+                mankind.
+              </p>
+            </div>
+            <div className="">
+              <img src={tree} alt="tree" />
+            </div>
           </div>
-          <div className="">
-            <img
-              src={tree}
-              alt="tree"
+
+          <div>
+            <div>
+            <svg
+              className="w-6 h-6 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 19l-7-7 7-7"
+              ></path>
+            </svg>
+            </div>
               
-            />
           </div>
         </div>
       </div>
