b4c5890cb5ca9268e488d981095030f06ba481f2
akshatgg <akshatg9636@gmail.com>
2024-04-17 02:15:36 +0545

    setting up the routing

--- BAckend/app.js
+++ BAckend/app.js
@@ -1,24 +1,32 @@
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const express = require("express");
-
-const app=express();2
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import morgan from "morgan"; // Import morgan module
+import userRoutes from "./routes/user.routes.js"
+const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:[process.env.CLIENT_URL],
-    credentials:true
-}))
+    origin: [process.env.CLIENT_URL],
+    credentials: true
+}));
+
+
+
+app.use('/api/v1/user',userRoutes);
+
+
+
 
 app.use(cookieParser());
+app.use(morgan('dev')); // Use morgan middleware
 
-app.use('/ping', function(req,res){
-  res.send('/pong');
-})
+app.use('/ping', function(req, res) {
+    res.send('/pong'); //for checking//
+});
 
-app.all('*',(req,res)=>{
+app.all('*', (req, res) => {
     res.status(404).send('OOPS! 404 page not found');
-})
+});
 
-module.exports=app;
-
+export default app;
--- BAckend/server.js
+++ BAckend/server.js
@@ -1,7 +1,7 @@
-const app=require('./app');
+import app from './app.js';
 
-const PORT =process.env || 5000;
-app.listen(PORT ,()=>{
+const PORT =process.env.PORT || 5000;
+app.listen(PORT ,async ()=>{
     console.log(`app is running:${PORT}` );
-    
+
 })
